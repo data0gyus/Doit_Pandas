b1c350cb874ef853f41a908a83042c1ab8c59309
data0gyus <dwjsk2008@gmail.com>
2025-04-22 17:49:55 +0900

    1일차 시도

--- code/ch02.py
+++ code/ch02.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/gapminder.tsv', sep='\t')
 
 print(type(df))
 
 print(df.shape)
+
+g = df.groupby('year')['lifeExp'].mean()
+print(g)
+g.plot()
+plt.show()
